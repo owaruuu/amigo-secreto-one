fb64504d807509a39a598dbabf4e60ddcd79146b
Josue Marquez <owaruuu@gmail.com>
2025-08-11 16:54:02 -0400

    Agregar codigo de javascript, agregar readme

---
+++ README.md
@@ -0,0 +1,31 @@
+# Amigo Secreto
+
+Esta pequena Pagina es un desafio para el curso ONE de Oracle en la plataforma de Alura LATAM.
+
+## Contenido
+
+-   [Resumen](#resumen)
+    -   [Puntos Claves](#puntos-claves)
+    -   [Links](#links)
+-   [Autor](#autor)
+
+## Resumen
+
+La pagina presenta un form para agregar el nombre de amigos, una vez que se tiene una lista se puede elegir al azar un de los amigos de la lista, los botones de los forms validan que el campo no este vacio o que la lista de amigos no este vacia.
+
+### Puntos Claves
+
+-   Funciones
+-   Condicionales
+-   Arrays
+-   Loops
+-   Modificacion del DOM
+-   Uso de Git
+
+### Links
+
+-   Live Site URL: [Live site](https://owaruuu.github.io/amigo-secreto-one/)
+
+## Autor
+
+-   Linkedin - [Josue Marquez](https://www.linkedin.com/in/josuemarquez/)
--- app.js
+++ app.js
@@ -1,1 +1,58 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+const elementoNuevoAmigo = document.getElementById("amigo");
+const listaAmigos = document.getElementById("listaAmigos");
+const resultado = document.getElementById("resultado");
+
+function agregarAmigo() {
+    const nuevoAmigo = elementoNuevoAmigo.value;
+
+    if (nuevoAmigo.trim() === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nuevoAmigo);
+    elementoNuevoAmigo.value = "";
+    elementoNuevoAmigo.focus();
+    actualizarLista();
+}
+
+function actualizarLista() {
+    resultado.innerHTML = "";
+    listaAmigos.innerHTML = "";
+
+    amigos.forEach((amigo) => {
+        createSimple("li", listaAmigos, amigo);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        return alert("Por favor, agrege amigos primero.");
+    }
+
+    const numeroAleatorio = intAleatorio(amigos.length);
+    const amigoSorteado = amigos[numeroAleatorio];
+
+    resultado.innerHTML = "";
+    listaAmigos.innerHTML = "";
+    amigos = [];
+
+    createSimple(
+        "li",
+        resultado,
+        `El amigo secreto sorteado es: ${amigoSorteado}`
+    );
+}
+
+function intAleatorio(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function createSimple(type, parent, content) {
+    const element = document.createElement(type);
+    parent.appendChild(element);
+    element.innerHTML = content;
+    return element;
+}
